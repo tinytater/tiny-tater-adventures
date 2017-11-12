c4dee467b897768cce2906cf50789fd373393cfe
Jeremy Yang <Jeremyyang920@users.noreply.github.com>
2017-11-12 15:21:39 -0800

    Rohan (#1)
    * rohan did this all
    
    * Viv's first commit ;)
    
    * Platform Colliders and Design Assets for Map
    
    * Update camera and max player movement
    
    * edit camera settings

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	Vector3 offset;
+
+    [SerializeField]
+    private float xMin;
+    [SerializeField]
+    private float xMax;
+
+    [SerializeField]
+    private float yMin;
+    [SerializeField]
+    private float yMax;
+
+    private Transform target;
+
+	void Start()
+	{
+        target = GameObject.Find("Player").transform;
+		//offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate()
+	{
+        transform.position = new Vector3(Mathf.Clamp(target.position.x, xMin, xMax), Mathf.Clamp(target.position.y, yMin, yMax),transform.position.z);
+		//transform.position = player.transform.position + offset;
+        //Debug.Log("Transform position " + transform.position);
+
+    }
+}
