cbed91243e6198861d5a13439e46fb516e92b4d3
Jeremy Yang <Jeremyyang920@users.noreply.github.com>
2017-11-20 16:57:15 -0800

    Jeremy (#2)
    * rohan did this all
    
    * Viv's first commit ;)
    
    * Platform Colliders and Design Assets for Map
    
    * Update camera and max player movement
    
    * edit camera settings
    
    * camera settings
    
    * added title screen for start button
    
    * added new assets and menu
    
    * Added credits screen

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void NewGameBtn(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+
+    public void CreditsBtn(string credits)
+    {
+        SceneManager.LoadScene(credits);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class PlayVideo : MonoBehaviour
+{
+
+    // Use this for initialization
+    public RawImage image;
+    //Video To Play [Assign from the Editor]
+    [SerializeField]
+    public VideoClip videoToPlay;
+
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
+
+    //Audio
+    private AudioSource audioSource;
+    void Start()
+    {
+        Application.runInBackground = true;
+        StartCoroutine(playVideo());
+    }
+
+    IEnumerator playVideo()
+    {
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+
+        videoPlayer.source = VideoSource.VideoClip;
+
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        videoPlayer.clip = videoToPlay;
+        videoPlayer.Prepare();
+
+        while (!videoPlayer.isPrepared)
+        {
+            Debug.Log("Preparing Video");
+            yield return null;
+        }
+
+        Debug.Log("Done Preparing Video");
+
+        image.texture = videoPlayer.texture;
+
+        videoPlayer.Play();
+
+        audioSource.Play();
+
+        Debug.Log("Playing Video");
+        while (videoPlayer.isPlaying)
+        {
+            Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            yield return null;
+        }
+
+        SceneManager.LoadScene("Title");
+
+        Debug.Log("Done Playing Video");
+    }
+}
