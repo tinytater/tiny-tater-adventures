96735862a6f696b2eb438f8a59cb1d2627573dac
Jeremy Yang <jeremy2@uci.edu>
2017-12-04 21:57:13 -0800

    added moving enemies

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour
+{
+    private Vector3 pointB;
+
+    IEnumerator Start()
+    {
+        var pointA = transform.position;
+        var pointB = new Vector3(pointA.x + 3, pointA.y, 0);
+        while (true)
+        {
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+        }
+    }
+
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i = 0.0f;
+        var rate = 1.0f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
+}
