3142c1a04d7dcea5e57d1b564db5151b06444ac0
Jeremy Yang <Jeremyyang920@users.noreply.github.com>
2017-12-05 21:36:31 -0800

    Pre Release #2 (#9)
    * added intro video
    
    * added intro as well as adjust sounds
    
    * intro vid
    
    * added updated pear coin
    
    * Added death animation and pause music on death
    
    * movespeed to 0 on death
    
    * added konami code and tagged all prefabs as enemies
    
    * updated bg music and space to skip movie
    
    * added water faucet and space/esc to return to menu from the ending
    scenes
    
    * added hitbox to sink
    
    * added 2 more pear coins
    
    * updated the intro movie
    
    * added space to skip intro video
    
    * fixed death boolean issue
    
    * added knife asset and window pear coins
    
    * added moving enemies
    
    * added deltax and delta y to enemy mobe
    
    * fixed window pane and made mouse turn around at end of movement

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField]
+    public int deltaX;
+    [SerializeField]
+    public int deltaY;
+    private Vector3 pointB;
+
+
+    IEnumerator Start()
+    {
+        var pointA = transform.position;
+        var pointB = new Vector3(pointA.x+deltaX , pointA.y+deltaY, 0);
+        while (true)
+        {
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
+            transform.Rotate(0, 180, 0);
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+            transform.Rotate(0, 180, 0);
+
+        }
+    }
+
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i = 0.0f;
+        var rate = 1.0f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
+}
